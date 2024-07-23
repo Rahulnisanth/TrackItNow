f2a5c4caccb9300aff398d42ba85e3a95269c9db
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-07-23 12:44:15 +0530

    added-navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,41 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+
+const nav_icons = [
+  { src: "/assets/icons/search.svg", alt: "search" },
+  { src: "/assets/icons/black-heart.svg", alt: "black-heart" },
+  { src: "/assets/icons/user.svg", alt: "user" },
+];
 
 const Navbar = () => {
   return (
-    <header>
-      <div>Navbar</div>
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+          <p className="nav-logo">
+            Price<span className="text-primary">Pulse</span>
+          </p>
+        </Link>
+
+        <div className="flex item-center gap-5">
+          {nav_icons.map((icon) => (
+            <Image
+              key={icon.alt}
+              src={icon.src}
+              alt={icon.alt}
+              width={28}
+              height={28}
+            />
+          ))}
+        </div>
+      </nav>
     </header>
   );
 };
