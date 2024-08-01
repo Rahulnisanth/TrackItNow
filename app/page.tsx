ecdd1f2d26e8b6b8f8b62171cf1c05f74bc1c296
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-08-01 21:48:23 +0530

    //renamed-app

--- app/page.tsx
+++ app/page.tsx
@@ -6,22 +6,13 @@ import SearchBar from "@/components/SearchBar";
 export default function Home() {
   return (
     <>
-    {/* Hero Section */}
+      {/* Hero Section */}
       <section className="px-6 md:px-20 py-10">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
-            <p key="first-para" className="small-text">
-              Smart Shopping Starts here
-              <Image
-                src="/assets/icons/arrow-right.svg"
-                alt="arrow-right"
-                height={16}
-                width={16}
-              />
-            </p>
             <h1 className="head-text">
               Unleash the power of
-              <span className="text-primary"> TrackItNow</span>.
+              <span className="text-primary"> TrackItNow.</span>
             </h1>
             <p key="second-para" className="mt-6">
               Powerful self-server product and growth analytics to help you
