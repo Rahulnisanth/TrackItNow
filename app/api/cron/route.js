0bf52b479d2739608839312849db3be240f0d2a0
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-11-11 12:22:56 +0530

    fixed the maxDuration or plan

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -10,7 +10,7 @@ import {
 } from "../../../lib/utils";
 import { generateEmailContent, sendEmail } from "../../../lib/nodemailer/index";
 
-export const maxDuration = 300;
+export const maxDuration = 60;
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
