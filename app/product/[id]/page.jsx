c9c03dba0699c12cecc1483bd6a01e34cc478112
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-11-11 11:55:13 +0530

    fix the email sending bug

--- app/product/[id]/page.jsx
+++ app/product/[id]/page.jsx
@@ -58,15 +58,6 @@ const ProductDetails = async ({ params: { id } }) => {
                   height={20}
                 />
               </div>
-
-              <div className="p-2 bg-white-200 rounded-10 cursor-pointer">
-                <Image
-                  src="/assets/icons/share.svg"
-                  alt="share"
-                  width={20}
-                  height={20}
-                />
-              </div>
             </div>
           </div>
 
